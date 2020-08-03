66809132ccf63dbcba1078d6d218b762bbe1b18a
memolinda <linda.giacomozzi@gmail.com>
2020-08-03 18:59:48 +0200

    back end:calculate the avarege of the users height and included in the email sent back to the user

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgred@localhost/height_collector' #connect to the postgresql app to the database with an address of: username@password.localhost/name_of_database (of postresql)
@@ -30,11 +31,16 @@ def success():
     if request.method == 'POST':
         email=request.form["email_address"]
         height=request.form["height"]
-        send_email(email, height)
+
         if db.session.query(Data).filter(Data.email_==email).count() == 0: #filtering the column if the email is already in the database
             data=Data(email,height) #initiate the class and the values will be recognise by the sqlalchemy add method
             db.session.add(data) #add the values to the database
             db.session.commit() #commit the changes to the database
+            avarege_height=db.session.query(func.avg(Data.height_)).scalar()
+            avarege_height=round(avarege_height,1)
+            count=db.session.query(Data.height_).count()
+            send_email(email, height,avarege_height, count) #use the send_email file to access to the credential of the email address used to send email to the user and the avarege_height
+
             return render_template("success.html")
     return render_template("index.html", text="Seems this email address was already used!")
 
